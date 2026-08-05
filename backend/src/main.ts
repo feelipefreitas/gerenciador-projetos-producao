f03ad71d68b4a9383c91f205d9e7fa56fefe15ee
feelipefreitas <feelipe.freitas97@gmail.com>
2026-08-05 19:41:52 -0300

    adiciona cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,5 @@
 import express from "express"
-// import cors from "cors"
+import cors from "cors"
 import authRoutes from "./routes/auth.routes"
 import projectRoutes from "./routes/projects.routes"
 import taskRoutes from "./routes/tasks.routes"
@@ -9,7 +9,7 @@ import { authMiddleware } from "./middleware/auth.middleware"
 
 const app = express()
 
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 
 // Simula latência de rede (300ms)
